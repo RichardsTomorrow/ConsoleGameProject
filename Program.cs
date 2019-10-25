095bb1ae7e6630a6faa61360af3f09ec1e5bbd96
Richard <richardwjmorrow@gmail.com>
2019-10-25 10:48:02 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
