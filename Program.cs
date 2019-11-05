11082ce5a6e6bf45e9b8a244972fe4ed429aa28a
Richard <richardwjmorrow@gmail.com>
2019-11-05 08:54:15 -0800

    deleted some old commented out color tests

--- Program.cs
+++ Program.cs
@@ -15,35 +15,6 @@ namespace ConsoleGameProject
             Drill drill = ColoringAndText.PlayerSetup();
             ColoringAndText.RosterColorAndText(drill);
             drill.Gameplay();
-            //ColoringAndText.Traveling(drill);
-
-            //ColoringAndText.Surface();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
-
-            //ColoringAndText.Soil();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
-
-            //ColoringAndText.Crust();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
-
-            //ColoringAndText.UpperMantle();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
-
-            //ColoringAndText.LowerMantle();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
-
-            //ColoringAndText.OuterCore();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
-
-            //ColoringAndText.InnerCore();
-            //Console.WriteLine("Testing this to see how it looks!!!___=+++=22");
-            //Console.ReadKey();
 
             //Console.WriteLine("\nend of test");
             //Console.ReadKey();
